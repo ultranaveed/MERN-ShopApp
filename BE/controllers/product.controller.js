6776c747a031159cfa6c98c0d1eaff45491f32c9
ultranaveed <naveednovo01@gmail.com>
2024-12-10 01:29:00 +0600

    Minor changes

--- BE/controllers/product.controller.js
+++ BE/controllers/product.controller.js
@@ -52,13 +52,18 @@ export const updateProduct = async (req,res) => {
 
 export const deleteProduct = async (req, res) =>{
     const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success:false, message: "Invalid Product ID."});
+    }
+
     try {
         await Product.findByIdAndDelete(id);
         res.status(200).json({success:true, message: "Product Deleted!"});
         console.log("Product deleted successfully");
     } catch (error) {
         console.log("Error in deleting product: ", error.message);
-        res.status(404).json({success:false, message: "Product Not Found!"});
+        res.status(500).json({success:false, message: "Server error!"});
 
     }
 };
--- BE/server.js
+++ BE/server.js
@@ -7,13 +7,15 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express(); // allows us to accept json data in the req.body
+const PORT = process.env.PORT
 
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () =>{
+app.listen(PORT, () =>{
+    console.log("What's up Novo");
     connectDB();
-    console.log("Server started at http://localhost:5000 What's up Novo");
+    console.log("Server started at http://localhost:"+ PORT);
     
 });
