3f0bbd05577b73218093b2075a94eced212d7af1
ultranaveed <naveednovo01@gmail.com>
2025-01-04 01:20:14 +0600

    Project Completed :D

--- BE/server.js
+++ BE/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from "dotenv";
+import path from "path"
 import cors from "cors"; // Import cors
 
 
@@ -11,11 +12,20 @@ dotenv.config();
 const app = express(); // allows us to accept json data in the req.body
 const PORT = process.env.PORT
 
+const __dirname = path.resolve();
+
 app.use(cors()); // Enable CORS for all routes
 
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/FE/dist")));
+    app.get("*", (req,res) =>{
+        res.sendFile(path.resolve(__dirname, "FE", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () =>{
     console.log("What's up Novo");
